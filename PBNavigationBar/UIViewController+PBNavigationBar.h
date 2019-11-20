d8beb3c2d334b56c0ee7f090c81408c8a09d5517
Jpache <jingpengbomail@gmail.com>
2019-11-20 11:46:57 +0800

    no message

--- PBNavigationBar/UIViewController+PBNavigationBar.h
+++ PBNavigationBar/UIViewController+PBNavigationBar.h
@@ -9,11 +9,34 @@
 #import <UIKit/UIKit.h>
 
 @interface UIViewController (PBNavigationBar)
-
+/**
+ self.navigationController.navigationBar.translucent;
+ */
+@property (nonatomic, assign) BOOL appearanceBarTranslucent;
+/**
+ [self.navigationController.navigationBar backgroundImageForBarMetrics:UIBarMetricsDefault];
+ */
+@property (nonatomic, strong) UIImage *navBarBackgroundImage;
+/**
+ self.navigationController.navigationBar.shadowImage;
+ */
+@property (nonatomic, strong) UIImage *navBarShadowImage;
+/**
+ self.navigationController.navigationBar.barTintColor;
+ */
+@property (nonatomic, assign) UIColor *navBarTintColor;
+/**
+ self.navigationController.navigationBar.barStyle;
+ */
+@property (nonatomic, assign) UIBarStyle navBarStyle;
+/**
+ self.navigationController.navigationBar.alpha;
+ */
+@property (nonatomic, assign) CGFloat navBarAlpha;
 /**
  self.navigationController.isNavigationBarHidden;
  */
-@property (nonatomic, assign, readonly) BOOL hiddenNavBar;
+@property (nonatomic, assign) BOOL hiddenNavBar;
 /**
  特殊处理的页面用的假 navBar
  */
